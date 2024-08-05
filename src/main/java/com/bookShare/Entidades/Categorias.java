5fb80c9674682d1373405616bbc1c10a1cb98ca3
JordiJT99 <jordijt.99@gmail.com>
2024-08-05 10:26:16 +0200

    editado calse categorias

--- src/main/java/com/bookShare/Entidades/Categorias.java
+++ src/main/java/com/bookShare/Entidades/Categorias.java
@@ -15,6 +15,9 @@ public class Categorias{
 
     private String name;
 
+    private String created_at;
+
+    private String updated_at;
 
 
     public Categorias() {}
@@ -26,6 +29,22 @@ public class Categorias{
 
     public Long getCategory_id() {
         return category_id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
     }
 
     public void setCategory_id(Long category_id) {
