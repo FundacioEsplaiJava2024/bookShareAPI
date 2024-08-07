8153ae5b910326e81e3e25e2507af6e4c40bfa86
JordiJT99 <jordijt.99@gmail.com>
2024-08-07 12:17:29 +0200

    se crea endpoint para logearse por email

--- src/main/java/com/bookShare/Controllers/UserController.java
+++ src/main/java/com/bookShare/Controllers/UserController.java
@@ -1,47 +1,59 @@
-    package com.bookShare.Controllers;
+package com.bookShare.Controllers;
 
-    import org.springframework.web.bind.annotation.*;
-    import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
-    import com.bookShare.Entidades.User;
+import com.bookShare.Entidades.User;
 import com.bookShare.Services.UserService;
 
 import java.util.List;
-    import java.util.Optional;
+import java.util.Optional;
 
-    @RestController
-    @CrossOrigin
-    @RequestMapping("/bookShare/users")
-    public class UserController {
+@RestController
+@CrossOrigin
+@RequestMapping("/bookShare/users")
+public class UserController {
 
-        @Autowired
-        private UserService userService;
+    @Autowired
+    private UserService userService;
 
-        @PostMapping("/add")
-        public User createUser(@RequestBody User user) {
-            return userService.createUser(user);
-        }
-
-        @GetMapping("/list")
-        public List<User> getAllUsers() {
-            return userService.getAllUsers();
-        }
-
-        @GetMapping("/{id}")
-        public Optional<User> getUserById(@PathVariable Long id) {
-            return userService.getUserById(id);
-        }
-
-        @PostMapping("/update/{id}")
-        public User updateUser(@PathVariable Long id, @RequestBody User user) {
-            return userService.updateUser(id, user);
-        }
-
-        @DeleteMapping("/delete/{id}")
-        public String deleteUser(@PathVariable Long id) {
-            System.out.println(id);
-            userService.deleteUser(id);
-            return "Eliminado correctamente";
-        }
-
+    @PostMapping("/add")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
     }
+
+    @GetMapping("/list")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/update/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        System.out.println(id);
+        userService.deleteUser(id);
+        return "Eliminado correctamente";
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        Optional<User> existingUser = userService.getUserByEmail(user.getEmail());
+        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+            return existingUser.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
+        }
+    }
+
+}
--- src/main/java/com/bookShare/Repositories/UserRepository.java
+++ src/main/java/com/bookShare/Repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.bookShare.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bookShare.Entidades.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
