ec7b9b84046a6646425c67d9ce08995cb77a7868
JordiJT99 <jordijt.99@gmail.com>
2024-08-14 10:30:18 +0200

    cambios en userController para coger el iddel user logeado por mail

--- src/main/java/com/bookShare/Controllers/UserController.java
+++ src/main/java/com/bookShare/Controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.bookShare.Controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import com.bookShare.Entidades.User;
 import com.bookShare.Services.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 import java.util.Optional;
@@ -45,15 +49,40 @@ public class UserController {
         userService.deleteUser(id);
         return "Eliminado correctamente";
     }
-
-    @PostMapping("/login")
-    public User login(@RequestBody User user) {
-        Optional<User> existingUser = userService.getUserByEmail(user.getEmail());
-        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
-            return existingUser.get();
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
-        }
+@SuppressWarnings("null")
+@PostMapping("/login")
+public LoginResponse login(@RequestBody User user) {
+    Optional<User> existingUser = userService.getUserByEmail(user.getEmail());
+    if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+        User authenticatedUser = existingUser.get();
+        Long userId = authenticatedUser.getUser_id();
+        // Guarda el ID del usuario en una variable de sesión
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        request.getSession().setAttribute("userId", userId);
+        return new LoginResponse(authenticatedUser, userId);
+    } else {
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
     }
+}
+
+class LoginResponse {
+    private User user;
+    private Long userId;
+
+    public LoginResponse(User user, Long userId) {
+        this.user = user;
+        this.userId = userId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
+    
+    
 
 }
