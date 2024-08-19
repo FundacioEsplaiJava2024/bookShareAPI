284508d55e9bf273fed9d13f6330e86453d8e200
JordiJT99 <jordijt.99@gmail.com>
2024-08-19 12:08:00 +0200

    añim

--- src/main/java/com/bookShare/Controllers/BookController.java
+++ src/main/java/com/bookShare/Controllers/BookController.java
@@ -1,15 +1,18 @@
 package com.bookShare.Controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bookShare.Entidades.Book;
 import com.bookShare.Services.BookService;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.UUID;  
+import java.io.File;
+
 
 
 @RestController
@@ -50,5 +53,16 @@ public class BookController {
     @GetMapping("/user/{userId}")
     public List<Book> getBooksByUserId(@PathVariable Long userId) {
         return bookService.getBooksByUserId(userId);
+    }
+
+     @PostMapping("/upload")
+    public String uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
+        String imageName = UUID.randomUUID().toString() + "_" + image.getOriginalFilename();
+        String imagePath = "public/books_images/" + imageName;
+
+        File file = new File(imagePath);
+        image.transferTo(file);
+       
+        return imageName; // Devuelve el nombre de la imagen para guardarla en la base de datos
     }
 }
