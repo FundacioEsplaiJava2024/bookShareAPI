088f9b4603419c21050f8b78e7b44ed1380783d2
AleXKiD31 <alexkid20025@gmail.com>
2024-08-20 13:44:59 +0200

    contacts

---
+++ src/main/java/com/bookShare/Services/ContactService.java
@@ -0,0 +1,43 @@
+package com.bookShare.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookShare.Entidades.Contacts;
+import com.bookShare.Repositories.ContactRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactsRepository;
+
+    public Contacts createContacts(Contacts contacts) {
+        return contactsRepository.save(contacts);
+    }
+
+    public List<Contacts> getAllContacts() {
+        return contactsRepository.findAll();
+    }
+
+    public Contacts updateContacts(Long contacts_id, Contacts contacts) {
+        contacts.setContact_id(contacts_id);
+        return contactsRepository.save(contacts);
+    }
+
+    public Optional<Contacts> getContactsById(Long contacts_id) {
+        return contactsRepository.findById(contacts_id);
+    }
+
+    public void deleteContacts(Long contacts_id) {
+        contactsRepository.deleteById(contacts_id);
+    }
+
+    public List<Contacts> getContactsByUserId(Long user_id) {
+        return contactsRepository.findByUserId(user_id);
+    }
+
+}
