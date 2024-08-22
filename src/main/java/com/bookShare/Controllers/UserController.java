0dbaef09a52adbec3e9d9d3b1ff86ea7217921c6
AleXKiD31 <alexkid20025@gmail.com>
2024-08-22 13:07:26 +0200

    updated security

--- src/main/java/com/bookShare/Controllers/UserController.java
+++ src/main/java/com/bookShare/Controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bookShare.Entidades.User;
 import com.bookShare.Requests.UpdateUserRequest;
@@ -29,6 +30,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @PostMapping("/add")
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
@@ -44,25 +48,24 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-
     @PostMapping("/update/{id}")
     public User updateUser(
             @PathVariable Long id,
             @RequestBody UpdateUserRequest request) throws IOException {
-    
+
         // Obtener el usuario existente
         Optional<User> userOptional = userService.getUserById(id);
         if (!userOptional.isPresent()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
         }
-    
+
         User user = userOptional.get();
-    
+
         // Actualizar campos según los parámetros que se proporcionan
         if (request.name != null) {
             user.setName(request.name);
         }
-       
+
         System.out.println("---------------------------------------------------------------------");
         System.out.println(request.imageUrl);
         if (request.imageUrl != null && !request.imageUrl.isEmpty()) {
@@ -70,13 +73,11 @@ public class UserController {
             System.out.println("---------------------------------------------------------------------");
             System.out.println(request.imageUrl);
         }
-    
+
         // Guardar los cambios en el usuario y verificar el resultado
         User updatedUser = userService.updateUser(id, user);
         return updatedUser; // Asegúrate de retornar el usuario actualizado
     }
-    
-
 
     @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable Long id) {
@@ -89,10 +90,10 @@ public class UserController {
     @PostMapping("/login")
     public LoginResponse login(@RequestBody User user) {
         Optional<User> existingUser = userService.getUserByEmail(user.getEmail());
-        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+        if (existingUser.isPresent()
+                && bCryptPasswordEncoder.matches(user.getPassword(), existingUser.get().getPassword())) {
             User authenticatedUser = existingUser.get();
             Long userId = authenticatedUser.getUser_id();
-            // Guarda el ID del usuario en una variable de sesión
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
                     .getRequest();
             request.getSession().setAttribute("userId", userId);
@@ -115,7 +116,7 @@ public class UserController {
         String currentDir = System.getProperty("user.dir");
         // Ruta relativa en el servidor del frontend
         String relativePath = Paths.get(currentDir, "..", "bookshare", "public", "users_images", imageName).toString();
-        
+
         File file = new File(relativePath);
         file.getParentFile().mkdirs(); // Asegúrate de que los directorios existen
         image.transferTo(file);
